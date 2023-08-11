1f219fe4fc7872101ffae13c04bdbf67648099c1
waitunsoe <waitunsoe.dev@gmail.com>
2023-08-11 15:18:32 +0630

    movie-slider

---
+++ script.js
@@ -0,0 +1,86 @@
+let menubar = document.querySelector('#menu-bar')
+let mynav = document.querySelector('.navbar')
+
+
+
+
+menubar.onclick = ()=>{
+  menubar.classList.toggle('fa-times')
+  mynav.classList.toggle('active')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let menubar = document.querySelector('#menu-bar');
+// let mycart = document.querySelector('#cart');
+// let opencart = document.querySelector('.add-cart');
+// let mynav = document.querySelector('.navbar');
+
+
+// menubar.onclick = () =>{
+//     menubar.classList.toggle('fa-times')
+//     mynav.classList.toggle('active')
+// }
+
+// mycart.onclick = () =>{
+
+//     opencart.classList.toggle('active')
+
+// }
+
